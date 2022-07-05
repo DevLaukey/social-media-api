8098826287d0777ac2f7f45b75464e3e1a7fae16
DevLaukey <devlaukeymwaura@gmail.com>
2022-07-05 16:12:38 +0300

    single comments created

--- controllers/postController.js
+++ controllers/postController.js
@@ -11,9 +11,7 @@ module.exports = {
       .input("post", url)
       .input("post_heading", heading)
       .input("post_description", description)
-      .input("post_comments", comments)
       .input("post_likes", likes)
-      .input("post_comments_replies", replies)
       .input("userID", userID)
       .execute(`dbo.add_posts`)
       .then((result) => {
@@ -21,8 +19,42 @@ module.exports = {
       })
       .then(console.log("Successfully added"))
       .catch((err) => {
-        console.log("sdvd" + err);
+        console.log(err);
       });
+  },
+  addComment: async function (req, res) {
+    let { comment, userID, postID } = req.body;
+    let { id } = req.params;
+    let pool = await poolPromise();
+    console.log(id);
+    if (id == postID) {
+      pool
+        .request()
+        .input("comment", comment)
+        .input("userID", userID)
+        .input("postID", postID)
+        .execute(`dbo.add_comments`)
+        .then((result) => {
+          result.rowsAffected &&
+            res.json({
+              status: 200,
+              success: true,
+              message: "comment added successfully",
+              results: result.recordset,
+            });
+        })
+        .then(console.log("Successfully added"))
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+       res.json({
+         status: 401,
+         success: false,
+         message: "unauthorized",
+         results: {},
+       });
+    }
   },
   viewPosts: async function (req, res) {
     let pool = await poolPromise();
@@ -40,5 +72,25 @@ module.exports = {
             message: "no records found",
           });
     });
+  },
+  viewComments: async function (req, res) {
+    let pool = await poolPromise();
+    const id = req.params.postId;
+    pool
+      .query(`SELECT * FROM comments WHERE postID ='${1}'`)
+      .then((results) => {
+        results.recordset.length
+          ? res.json({
+              status: 200,
+              success: true,
+              message: "success",
+              results: results.recordset,
+            })
+          : res.json({
+              status: 200,
+              success: false,
+              message: "no records found",
+            });
+      });
   },
 };
--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const { signUp, login } = require("../controllers/userController");
-const { addPost, viewPosts } = require("../controllers/postController");
+const {
+  addPost,
+  viewPosts,
+  addComment,
+  viewComments,
+} = require("../controllers/postController");
+
 const router = express.Router();
 
 router.get("/", viewPosts);
 
 router.post("/register", signUp);
 router.post("/login", login);
+
+router.post("/add-comments/:id", addComment);
+router.get("/post/comments/:postID", viewComments);
 router.post("/add-posts", addPost);
 
 module.exports = router;
